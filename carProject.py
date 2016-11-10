be07dfce160cd3197c13337b6e134b62b6fd9595
IYamAHobo <elliotflores1@gmail.com>
2016-11-10 15:18:00 -0600

    Update carProject.py

--- carProject.py
+++ carProject.py
@@ -1,5 +1,15 @@
 # We'll use this as the main file we use for the program.
-
+FILE = "Insert filepath here"
 def main():
 
+  
+  print("Welcome to the car program.")
+  try:
+    f = open(FILE, "r")
+  except:
+    print("You need to make a new file. Creating...")
+    f = open(FILE, "w")
+  
+  
+  
 main()
