8891338be6744449c53fbbb2297ddf10ac42bf52
IYamAHobo <elliotflores1@gmail.com>
2016-11-10 15:44:39 -0600

    Update carProject.py

--- carProject.py
+++ carProject.py
@@ -2,7 +2,10 @@
 FILE = "Insert filepath here"
 def main():
 
+  isInProgam = True
+  isPlacingOrder = False
   
+  while(isInProgram == True) :
   print("Welcome to the car program.")
   try:
     f = open(FILE, "r")
@@ -14,5 +17,27 @@ def main():
   print("1) Place an order")
   print("2) See your order")
   print("3) Exit program")
+  
+  try:
+    mainSelect = int(input())
+  except:
+    print("Invalid input. Please enter a number that corresponds to the related option.\n")
+    
+  # THE CAR SELECTION CODE STARTS HERE
+  if (mainSelect == 1) :
+    isPlacingOrder = True
+    while (isPlacingOrder == True) :
+      # Print your options for each car here.
+      # This will be where all the options, makers, and models will be chosen and displayed.
+      
+  if (mainSelect == 2) :
+      # Read the file in this section.
+      # Print out the contents of the file, and then close the file.
+      
+  if (mainSelect == 3) :
+    exit()
+      
+  else :
+    print("Invalid input. Please enter a number that corresponds to the related option.\n")
   
 main()
